388fbdc5419e6047e62a48fb84b662c5fbf66896
NightSkumbry <night_skumbry@outlook.com>
2023-10-07 18:40:22 +0700

    d

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from main_window_ui import Ui_MainWindow
 from auth_ui import Ui_Auth_dialog
 from configure_ui import Ui_Configure_dialog
+from redact_account_ui import Ui_Redact_account_dialog
 import sys
 import os
 from PyQt6 import QtCore, QtGui, QtWidgets
 import requests
 from constants import server_address
+from enum import Enum, auto
+
+
+class Stages(Enum):
+    Main = auto()
+    Auth = auto()
+    Configure = auto()
+    Redact_account = auto()
+
+
+def main_redact_account():
+    if global_stages[0] == Stages.Main:
+        window_redact_account.show()
+        global_stages[0] = Stages.Redact_account
+    
 
 
 def auth_login():
@@ -22,11 +38,11 @@ def auth_login():
             cookies = response.cookies
             window_auth.hide()
             if response.text == 'not changed':
-                global_stages[0] = 'configure'
+                global_stages[0] = Stages.Configure
                 window_configure.show()
                 ui_configure.Login_text.setText(ui_auth.Login_text.text())
             else:
-                global_stages[0] = 'main'
+                global_stages[0] = Stages.Main
                 window_main.show()
         else:
             ui_auth.Status_label.setText('Данные неверны')
@@ -49,7 +65,7 @@ def configure_save():
     if response.text == 'incorrect_login':
         ui_configure.Status_label.setText('Логин уже занят')
     else:
-        global_stages[0] = 'main'
+        global_stages[0] = Stages.Main
         window_configure.hide()
         window_main.show()
 
@@ -61,7 +77,14 @@ def configure_text_changed():
         ui_configure.Save_button.setEnabled(False)
 
 
-global_stages = ['auth']
+def redact_account_text_changed():
+    if ui_configure.Password_text.text() and ui_configure.Login_text.text():
+        ui_configure.Save_button.setEnabled(True)
+    else:
+        ui_configure.Save_button.setEnabled(False)
+
+
+global_stages = [Stages.Auth]
 cookies = False
 
 
@@ -71,17 +94,36 @@ app = QtWidgets.QApplication(sys.argv)
 window_main = QtWidgets.QMainWindow()
 ui_main_window = Ui_MainWindow()
 ui_main_window.setupUi(window_main)
+
 window_auth = QtWidgets.QDialog()
 ui_auth = Ui_Auth_dialog()
 ui_auth.setupUi(window_auth)
+
 window_configure = QtWidgets.QDialog()
 ui_configure = Ui_Configure_dialog()
 ui_configure.setupUi(window_configure)
 
+def close_window():
+    print('also works correctly')
+
+# window_redact_account = QtWidgets.QDialog()
+class WindowRedactAccount(QtWidgets.QWidget):
+    def __init__(self):
+        super(WindowRedactAccount, self).__init__(None)
+    def closeEvent(self, event):
+        print('closed')
+        close_window()
+window_redact_account = WindowRedactAccount()    
+ui_redact_account = Ui_Redact_account_dialog()
+ui_redact_account.setupUi(window_redact_account)
+
 # functional
+ui_main_window.Redact_account_button.clicked.connect(main_redact_account)
+
 ui_auth.Auth_button.clicked.connect(auth_login)
 ui_auth.Login_text.textChanged.connect(auth_text_changed)
 ui_auth.Password_text.textChanged.connect(auth_text_changed)
+
 ui_configure.Login_text.textChanged.connect(configure_text_changed)
 ui_configure.Password_text.textChanged.connect(configure_text_changed)
 ui_configure.Save_button.clicked.connect(configure_save)
